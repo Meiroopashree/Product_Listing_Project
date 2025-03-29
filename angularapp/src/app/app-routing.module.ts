f66c95720f40a7c7dfd0cb60506002a1fd353148
Meiroopashree <122288539+Meiroopashree@users.noreply.github.com>
2025-03-29 17:20:03 +0530

    Update app-routing.module.ts

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListingComponent } from './product-listing/product-listing.component';
 
-
 const routes: Routes = [
-  {path: '', component: ProductListingComponent}
+  { path: '', component: ProductListingComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),{ useHash: true }],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], 
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
